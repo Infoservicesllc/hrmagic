f5a997fc680b8baf36a4b4ba47a7da7a1f6bcf19
afeefah <User@HP.gateway.pace.com>
2016-04-02 21:36:23 -0400

    third commit..!

--- hrmagiccore/src/main/java/com/hrmagiccore/App.java
+++ hrmagiccore/src/main/java/com/hrmagiccore/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("HR Project!! :) ");
+        System.out.println("HR Application!! :) ");
+        System.out.println("for InfoServices..");
     }
 }
