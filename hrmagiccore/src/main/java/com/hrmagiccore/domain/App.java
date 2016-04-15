4082d8f0e54c4d4e64ba0b3cb83b53bc2d8310aa
afeefah <User@HP>
2016-04-15 17:00:14 -0400

    test commit from new workspace..

---
+++ hrmagiccore/src/main/java/com/hrmagiccore/domain/App.java
@@ -0,0 +1,16 @@
+package com.hrmagiccore.domain;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("InfoServices HR Application!!");
+        System.out.println("test!");
+    }
+}
