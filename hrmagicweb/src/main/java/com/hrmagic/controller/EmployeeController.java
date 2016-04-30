4ba4b0b6e0a0b8098f27fcc65a6d62eb91a47115
afeefah <User@HP.gateway.pace.com>
2016-04-30 18:26:39 -0400

    Emp Controller

--- hrmagicweb/src/main/java/com/hrmagic/controller/EmployeeController.java
+++ hrmagicweb/src/main/java/com/hrmagic/controller/EmployeeController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,40 +15,36 @@ import com.hrmagiccore.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
-	
-	
-	@Autowired
-	private EmployeeService employeeService;
 
-	@RequestMapping(value = "/getEmployeeList", method = RequestMethod.GET)
-	public ResponseEntity<List<Employee>> getEmployee() {
-		System.out.println("getlist  method");
-		ResponseEntity<List<Employee>> responseEntity = new ResponseEntity<List<Employee>>(employeeService.getEmployeeList(),HttpStatus.OK);
-		return responseEntity;
+	private Employee employee;
+	@Autowired
+	EmployeeService employeeService;
+
+	@RequestMapping(value = "/employees", method = RequestMethod.GET)
+	public ResponseEntity<List<Employee>> getEmployees() {
+		ResponseEntity<List<Employee>> resp  = new ResponseEntity<List<Employee>>(employeeService.getEmployeeList(),HttpStatus.OK);
+		return resp ;
 	}
 
-	@RequestMapping(value = "/getEmployee", method = RequestMethod.GET)
+	@RequestMapping(value = "/employees/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Employee> getEmployee(@PathVariable int empID) {
 		ResponseEntity<Employee> entity = new ResponseEntity<Employee>(employeeService.getEmployee(empID), HttpStatus.OK);
 		return entity; 
 	}
     
-	@RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
-	public ResponseEntity<Boolean> addEmployee(@RequestBody Employee emp) {
-		 emp = null;
-		 System.out.println("add method");
-		 
+	@RequestMapping(value = "/employees", method = RequestMethod.POST)
+	public ResponseEntity<Boolean> addEmployee(@PathVariable Employee emp) {
 		ResponseEntity<Boolean> entity = new ResponseEntity<Boolean>(employeeService.addEmployee(emp), HttpStatus.CREATED);
 		return entity; 
 	}
 	
-	@RequestMapping(value = "/deleteEmployee", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/employees/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Boolean> deleteEmployee(@PathVariable int empID) {
 		ResponseEntity<Boolean> entity = new ResponseEntity<Boolean>(employeeService.deleteEmployee(empID), HttpStatus.OK);
 		return entity; 
 	}
 
-	@RequestMapping(value = "/updateEmployee", method = RequestMethod.PUT)
+	@RequestMapping(value = "/employees/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Boolean> updateEmployee(@PathVariable Employee emp) {
 		ResponseEntity<Boolean> entity = new ResponseEntity<Boolean>(employeeService.addEmployee(emp), HttpStatus.OK);
 		return entity;
